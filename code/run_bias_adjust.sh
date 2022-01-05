4b5fd64e637d0f9ecf35d59c424cd9b6c8eca292
Bijan Fallah <fallah@pik-potsdam.de>
2022-01-05 08:38:29 +0100

    editted the echo commands

--- code/run_bias_adjust.sh
+++ code/run_bias_adjust.sh
@@ -5,17 +5,17 @@ do
     echo "Variable is "$var 
     for scenario in ssp126 ssp370 ssp585
     do 
-        echo "scenario is "$scenario
+        echo "  scenario is "$scenario
         for mod in GFDL-ESM4  IPSL-CM6A-LR  MPI-ESM1-2-HR  MRI-ESM2-0  UKESM1-0-LL
         do 
-            echo "model is "$mod
+            echo "    model is "$mod
             model=$(echo "$mod" | tr '[:upper:]' '[:lower:]')
             for time_slice in historical near_future middle_future far_future
             do 
-                echo "time_slice is "$time_slice
-                echo "sending the job to slurm !"
+                echo "      time_slice is "$time_slice
+                echo "         sending the job to slurm !"
                 sbatch bias_adjust.sh $var $scenario $model $time_slice 
-                echo "waiting 3 minutes to send the next job! "
+                echo "            waiting 3 minutes to send the next job! "
                 sleep 180
             done 
         done
